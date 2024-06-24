983f89b368be86a7f98cb4b12fd22532697ead1f
tomato <tomatomato2121@gmail.com>
2024-06-24 23:35:45 +0900

    create all table from seoul open data

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from urllib.parse import quote
 from sqlalchemy import create_engine, text, MetaData
 from dotenv import dotenv_values
-from model.seoul_commercial_model import CommercialChangeIndicatorModel
-from model.seoul_population_model import PopulationStreetModel
+from model.seoul_commercial_model import CommercialChangeIndicatorModel, CommercialExpenditureModel, CommercialSalesInfoModel, CommercialBusinessInfoModel, CommercialFacilityInfoModel
+from model.seoul_locatation_model import LocAdministrativeDistrictModel, LocApartmentInfoModel
+from model.seoul_population_model import PopulationResidentModel, PopulationStreetModel, PopulationWorkplaceModel
 
 # take environment variables from .env
 config = dotenv_values("DB.env")
@@ -24,9 +25,17 @@ engine = create_engine(connection_string)
 metadata = MetaData()
 
 # Table 생성
-population_street = PopulationStreetModel(metadata)
-commercial_change_indicator = CommercialChangeIndicatorModel(metadata)
-
+commercial_change_indicator_table = CommercialChangeIndicatorModel(metadata)
+commercial_expanditure_table = CommercialExpenditureModel(metadata)
+commercial_sales_info_table = CommercialSalesInfoModel(metadata)
+commercial_bussiness_info_table = CommercialBusinessInfoModel(metadata)
+loc_apartment_info_table = LocApartmentInfoModel(metadata)
+loc_administrative_district_table = LocAdministrativeDistrictModel(metadata)
+loc_facility_info_table = CommercialFacilityInfoModel(metadata)
+population_street_table = PopulationStreetModel(metadata)
+population_resident_table = PopulationResidentModel(metadata)
+population_workplace_table = PopulationWorkplaceModel(metadata)
 metadata.create_all(engine)
+
 
 # 각 테이블에 데이터 생성
--- model/seoul_population_model.py
+++ model/seoul_population_model.py
@@ -8,12 +8,9 @@ class PopulationStreetModel:
             # 상권 코드
             Column('TRDAR_CD', String, nullable=False, comment="상권_코드"),
             Column('TRDAR_CD_NM', String, nullable=False, comment="상권_코드_명"),
-
-            Column('STDR_YYQU_CD', String, nullable=False, comment="기준_년분기_코드"),
-
-            # 상권 구분 코드
             Column('TRDAR_SE_CD', String, nullable=False, comment="상권_구분_코드"),
             Column('TRDAR_SE_CD_NM', String, nullable=False, comment="상권_구분_코드_명"),
+            Column('STDR_YYQU_CD', String, nullable=False, comment="기준_년분기_코드"),
 
             # 남성 유동인구 수
             Column('ML_FLPOP_CO', Integer, nullable=False, comment="남성_유동인구_수"),
@@ -46,4 +43,96 @@ class PopulationStreetModel:
             Column('TMZON_14_17_FLPOP_CO', Integer, nullable=False, comment="시간대_14_17_유동인구_수"),
             Column('TMZON_17_21_FLPOP_CO', Integer, nullable=False, comment="시간대_17_21_유동인구_수"),
             Column('TMZON_21_24_FLPOP_CO', Integer, nullable=False, comment="시간대_21_24_유동인구_수"),
+        )
+
+# 상주인구
+class PopulationResidentModel:
+    def __init__(self, metadata):
+        self.resident_population = Table(
+            'population_resident', metadata,
+
+            # 상권 코드
+            Column('TRDAR_CD', String, nullable=False, comment="상권_코드"),
+            Column('TRDAR_CD_NM', String, nullable=False, comment="상권_코드_명"),
+            Column('TRDAR_SE_CD', String, nullable=False, comment="상권_구분_코드"),
+            Column('TRDAR_SE_CD_NM', String, nullable=False, comment="상권_구분_코드_명"),
+            Column('STDR_YYQU_CD', String, nullable=False, comment="기준_년분기_코드"),
+
+            Column('TOT_REPOP_CO', Integer, nullable=False, comment="총_상주인구_수"),
+            # 남성 연령대별 상주인구 수
+            Column('MAG_10_REPOP_CO', Integer, nullable=False, comment="남성연령대_10_상주인구_수"),
+            Column('MAG_20_REPOP_CO', Integer, nullable=False, comment="남성연령대_20_상주인구_수"),
+            Column('MAG_30_REPOP_CO', Integer, nullable=False, comment="남성연령대_30_상주인구_수"),
+            Column('MAG_40_REPOP_CO', Integer, nullable=False, comment="남성연령대_40_상주인구_수"),
+            Column('MAG_50_REPOP_CO', Integer, nullable=False, comment="남성연령대_50_상주인구_수"),
+            Column('MAG_60_ABOVE_REPOP_CO', Integer, nullable=False, comment="남성연령대_60_이상_상주인구_수"),
+
+            # 여성 연령대별 상주인구 수
+            Column('FAG_10_REPOP_CO', Integer, nullable=False, comment="여성연령대_10_상주인구_수"),
+            Column('FAG_20_REPOP_CO', Integer, nullable=False, comment="여성연령대_20_상주인구_수"),
+            Column('FAG_30_REPOP_CO', Integer, nullable=False, comment="여성연령대_30_상주인구_수"),
+            Column('FAG_40_REPOP_CO', Integer, nullable=False, comment="여성연령대_40_상주인구_수"),
+            Column('FAG_50_REPOP_CO', Integer, nullable=False, comment="여성연령대_50_상주인구_수"),
+            Column('FAG_60_ABOVE_REPOP_CO', Integer, nullable=False, comment="여성연령대_60_이상_상주인구_수"),
+
+            # 연령대별 상주인구 수
+            Column('AGRDE_10_REPOP_CO', Integer, nullable=False, comment="연령대_10_상주인구_수"),
+            Column('AGRDE_20_REPOP_CO', Integer, nullable=False, comment="연령대_20_상주인구_수"),
+            Column('AGRDE_30_REPOP_CO', Integer, nullable=False, comment="연령대_30_상주인구_수"),
+            Column('AGRDE_40_REPOP_CO', Integer, nullable=False, comment="연령대_40_상주인구_수"),
+            Column('AGRDE_50_REPOP_CO', Integer, nullable=False, comment="연령대_50_상주인구_수"),
+            Column('AGRDE_60_ABOVE_REPOP_CO', Integer, nullable=False, comment="연령대_60_이상_상주인구_수"),
+
+            # 성별 상주인구 수
+            Column('ML_REPOP_CO', Integer, nullable=False, comment="남성_상주인구_수"),
+            Column('FML_REPOP_CO', Integer, nullable=False, comment="여성_상주인구_수"),
+
+            # 가구수
+            Column('APT_HSHLD_CO', Integer, nullable=False, comment="아파트_가구_수"),
+            Column('NON_APT_HSHLD_CO', Integer, nullable=False, comment="비_아파트_가구_수"),
+            Column('TOT_HSHLD_CO', Integer, nullable=False, comment="총_가구_수"),
+        )
+
+# 직장 인구
+class PopulationWorkplaceModel:
+    def __init__(self, metadata):
+        self.workplace_population = Table(
+            'population_workplace', metadata,
+
+            # 상권 관련 정보
+            Column('TRDAR_CD', String, nullable=False, comment="상권_코드"),
+            Column('TRDAR_CD_NM', String, nullable=False, comment="상권_코드_명"),
+            Column('TRDAR_SE_CD', String, nullable=False, comment="상권_구분_코드"),
+            Column('TRDAR_SE_CD_NM', String, nullable=False, comment="상권_구분_코드_명"),
+            Column('STDR_YYQU_CD', String, nullable=False, comment="기준_년분기_코드"),
+
+            Column('TOT_WRC_POPLTN_CO', Integer, nullable=False, comment="총_직장_인구_수"),
+
+            # 남성 연령대별 직장 인구 수
+            Column('MAG_10_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_10_직장_인구_수"),
+            Column('MAG_20_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_20_직장_인구_수"),
+            Column('MAG_30_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_30_직장_인구_수"),
+            Column('MAG_40_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_40_직장_인구_수"),
+            Column('MAG_50_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_50_직장_인구_수"),
+            Column('MAG_60_ABOVE_WRC_POPLTN_CO', Integer, nullable=False, comment="남성연령대_60_이상_직장_인구_수"),
+
+            # 여성 연령대별 직장 인구 수
+            Column('FAG_10_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_10_직장_인구_수"),
+            Column('FAG_20_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_20_직장_인구_수"),
+            Column('FAG_30_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_30_직장_인구_수"),
+            Column('FAG_40_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_40_직장_인구_수"),
+            Column('FAG_50_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_50_직장_인구_수"),
+            Column('FAG_60_ABOVE_WRC_POPLTN_CO', Integer, nullable=False, comment="여성연령대_60_이상_직장_인구_수"),
+
+            # 연령대별 직장 인구 수
+            Column('AGRDE_10_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_10_직장_인구_수"),
+            Column('AGRDE_20_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_20_직장_인구_수"),
+            Column('AGRDE_30_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_30_직장_인구_수"),
+            Column('AGRDE_40_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_40_직장_인구_수"),
+            Column('AGRDE_50_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_50_직장_인구_수"),
+            Column('AGRDE_60_ABOVE_WRC_POPLTN_CO', Integer, nullable=False, comment="연령대_60_이상_직장_인구_수"),
+
+            # 성별 직장 인구 수
+            Column('ML_WRC_POPLTN_CO', Integer, nullable=False, comment="남성_직장_인구_수"),
+            Column('FML_WRC_POPLTN_CO', Integer, nullable=False, comment="여성_직장_인구_수"),
         )
